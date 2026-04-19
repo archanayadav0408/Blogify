046d9f6f75f9cb637e3194525528851fc16ab235
Archana Yadav <archanayadav040896@gmail.com>
2026-04-19 13:33:46 +0530

    settings allowed host

--- Blogify/settings.py
+++ Blogify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8rb(!1ew%#u*ix#-#4ji62h^p58#pnkqn6e)0#@63tpklv&$b2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
